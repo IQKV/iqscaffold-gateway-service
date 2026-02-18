9cd554de35bb96eeb21e3db1e1c1f594bab47acb
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-02-18 12:31:09 +0200

    chore: update MeterRegistryCustomizer import to new package

--- src/main/java/com/iqscaffold/gatewayservice/config/MetricsConfig.java
+++ src/main/java/com/iqscaffold/gatewayservice/config/MetricsConfig.java
@@ -2,8 +2,8 @@ package com.iqscaffold.gatewayservice.config;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.config.MeterFilter;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.micrometer.metrics.autoconfigure.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;

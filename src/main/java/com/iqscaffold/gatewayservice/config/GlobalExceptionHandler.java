038500cdea988e66879c709bc4da3de735a5182a
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-02-18 12:19:26 +0200

    fix(config): update import for `ErrorWebExceptionHandler` to `webflux` package

--- src/main/java/com/iqscaffold/gatewayservice/config/GlobalExceptionHandler.java
+++ src/main/java/com/iqscaffold/gatewayservice/config/GlobalExceptionHandler.java
@@ -14,7 +14,7 @@ import com.iqscaffold.gatewayservice.exception.UnsupportedApiVersionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.boot.webflux.error.ErrorWebExceptionHandler;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
